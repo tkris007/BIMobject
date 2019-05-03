017bbaa602a40b1fdefe903569da515c980b20de
Tóth Kristóf <tkris@vipmail.hu>
2019-05-03 15:25:20 +0200

    Add solution

--- source/hasTriangles.cpp
+++ source/hasTriangles.cpp
@@ -1,8 +1,50 @@
 #include "hasTriangles.h"
 
 
-bool hasTriangles(const std::vector<int> &coords)
+bool isTriangle(int aSide, int bSide, int cSide)
 {
+	if(!(aSide + bSide > cSide))
+	{
+		return false;
+	}
+
+	if(!(aSide + cSide > bSide))
+	{
+		return false;
+	}
+
+	if(!(cSide + bSide > aSide))
+	{
+		return false;
+	}
+
+	return true;
+}
+
+
+bool hasTriangles(const std::vector<int> &sides)
+{
+	if(sides.size() < 3)
+	{
+		return false;
+	}
+
+	for(size_t i = 0; i < sides.size() - 2; ++i)
+	{
+		for(size_t j = i + 1; j < sides.size() - 1; ++j)
+		{
+			for(size_t k = j + 1; k < sides.size(); ++k)
+			{
+				if(!isTriangle(sides[i], sides[j], sides[k]))
+				{
+					continue;
+				}
+
+				return true;
+			}
+		}
+	}
+
 	return false;
 }
 
--- tests/tests.cpp
+++ tests/tests.cpp
@@ -6,10 +6,67 @@
 
 
 TEST(hasTriangle,
-     Given_an_array_with_size_lesser_than_3_When_checking_for_triangles_Then_returns_false)
+     Given_lesser_than_3_elements_When_checking_for_triangles_Then_returns_false)
 {
 	ASSERT_THAT(hasTriangles({}), false);
+	ASSERT_THAT(hasTriangles({1}), false);
+	ASSERT_THAT(hasTriangles({1, 2}), false);
 }
+
+TEST(hasTriangle,
+     Given_3_elements_as_a_triangle_When_checking_for_triangles_Then_returns_true)
+{
+	ASSERT_THAT(hasTriangles({2, 3, 4}), true);
+}
+
+TEST(hasTriangle,
+     Given_3_elements_and_first_plus_second_is_not_greater_than_the_third_When_checking_for_triangles_Then_returns_false)
+{
+	ASSERT_THAT(hasTriangles({1, 2, 3}), false);
+}
+
+TEST(hasTriangle,
+     Given_3_elements_and_second_plus_third_is_not_greater_than_the_first_When_checking_for_triangles_Then_returns_false)
+{
+	ASSERT_THAT(hasTriangles({3, 2, 1}), false);
+}
+
+TEST(hasTriangle,
+     Given_3_elements_and_third_plus_first_is_not_greater_than_the_second_When_checking_for_triangles_Then_returns_false)
+{
+	ASSERT_THAT(hasTriangles({1, 3, 2}), false);
+}
+
+TEST(hasTriangle,
+     Given_5_elements_a_triangele_in_first_second_and_last_value_When_checking_for_triangles_Then_returns_true)
+{
+	ASSERT_THAT(hasTriangles({2, 3, 0, 0, 4}), true);
+}
+
+TEST(hasTriangle,
+     Given_5_elements_a_triangele_in_first_middle_and_last_value_When_checking_for_triangles_Then_returns_true)
+{
+	ASSERT_THAT(hasTriangles({2, 0, 3, 0, 4}), true);
+}
+
+TEST(hasTriangle,
+     Given_5_elements_a_triangele_in_second_middle_and_last_value_When_checking_for_triangles_Then_returns_true)
+{
+	ASSERT_THAT(hasTriangles({0, 2, 3, 0, 4}), true);
+}
+
+TEST(hasTriangleAcceptance,
+     Given_the_example_data_with_triangles_in_it_When_checking_for_triangles_Then_returns_true)
+{
+	ASSERT_THAT(hasTriangles({10, 2, 5, 1, 8, 20}), true);
+}
+
+TEST(hasTriangleAcceptance,
+     Given_the_example_data_with_no_triangles_in_it_When_checking_for_triangles_Then_returns_true)
+{
+	ASSERT_THAT(hasTriangles({10, 50, 5, 1}), false);
+}
+
 
 int main(int argc, char **argv)
 {
